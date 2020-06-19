a8e94ad4263d30c4fe16969e9a39294fd3b72931
Costpap <costpapcostpap7@gmail.com>
2020-06-19 12:48:40 +0300

    feat: Make info command automatically get dev info

--- commands/info.js
+++ commands/info.js
@@ -1,19 +1,22 @@
 const Discord = require('discord.js');
 const humanizeDuration = require('humanize-duration');
-const { author, version } = require('../package.json');
+const { botOwner } = require('../config.json');
+const { version } = require('../package.json');
 
 module.exports = {
 	name: 'info',
 	description: 'Sends you information about the bot.',
 	aliases: ['information'],
 	cooldown: 5,
-	execute(message) {
+	async execute(message) {
+		const developer = await message.client.users.cache.get(botOwner);
+
 		const embed = new Discord.MessageEmbed()
 			.setColor('#6293f5')
 			.setThumbnail(message.client.user.displayAvatarURL({ format: 'png', dynamic: true }))
 			.setTitle(`${message.client.user.username} Information`)
 			.addFields(
-				{ name: 'Developer:', value: author },
+				{ name: 'Developer:', value: `${developer.tag} (${developer.id})` },
 				{ name: 'Version:', value: version, inline: true },
 				{ name: 'Library:', value: 'discord.js', inline: true },
 				{ name: 'Server Count:', value: message.client.guilds.cache.size, inline: true },
