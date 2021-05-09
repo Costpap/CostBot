cca7b63b71b0ff73367bb83ac4bfae64741bccf0
Forgi <42557352+Forgi4G@users.noreply.github.com>
2021-05-09 13:18:48 -0700

    Add checks
    The avatar command seemed to not work when there was no user specified, the new code in lines 11 - 13 guarantee there will be a user that the avatar command will work on. The "click to see avatar" link will also try to now get the avatar at a high resolution with a query param, the main image of the embed will be 1024x1024 so it doesn't take up the whole chat.

--- src/commands/avatar.ts
+++ src/commands/avatar.ts
@@ -8,13 +8,15 @@ export default {
     permissions: ['EMBED_LINKS'],
     cooldown: 5,
     do: async (message: Message, client: Client, args: string[], Discord: typeof import('discord.js')) => {
-        const user: User = parseUserMention(args[0], client) || client.users.cache.get(args[0]) || message.author;
+        let user: User;
+        if (!args[0]) user = message.author;
+        else user = parseUserMention(args[0], client) || client.users.cache.get(args[0]);
 
         const embed = new Discord.MessageEmbed()
             .setColor('RANDOM')
             .setAuthor(user.tag, user.displayAvatarURL({ format: 'png', dynamic: true }))
-            .setDescription(`[Click here for URL](${user.displayAvatarURL({ format: 'png', dynamic: true })})`)
-            .setImage(user.displayAvatarURL({ format: 'png', dynamic: true }))
+            .setDescription(`[Click here for URL](${user.displayAvatarURL({ format: 'png', dynamic: true })}?size=4096)`)
+            .setImage(user.displayAvatarURL({ format: 'png', dynamic: true }) + "?size=1024")
             .setTimestamp()
             .setFooter(
                 `Requested by ${message.author.tag}`,
