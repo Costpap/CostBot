5d99889e76a9c81e1f4c1f16106cea8335aa3cea
Costpap <costpapcostpap7@gmail.com>
2021-07-05 03:00:01 +0300

    refactor(logs): refine client-emitted error/warn logs

--- src/events/error.ts
+++ src/events/error.ts
@@ -1,7 +1,15 @@
 import { errorLog } from '../utils/logs';
-import { Client } from 'discord.js';
+import { Client, MessageEmbed, version } from 'discord.js';
 
 export default async (_Discord: typeof import('discord.js'), client: Client, error: string) => {
     console.error(error);
-    errorLog(error, client);
+
+    const embed = new MessageEmbed()
+        .setColor('RED')
+        .setTitle(`${client.user.username} Client-Emitted Error`)
+        .setDescription(`\`\`\`${error}\`\`\``)
+        .setTimestamp()
+        .setFooter(`Error occurred while running discord.js v${version}.`);
+
+    errorLog(embed, client, { noWebhook: true });
 };
--- src/events/warn.ts
+++ src/events/warn.ts
@@ -1,7 +1,15 @@
 import { errorLog } from '../utils/logs';
-import { Client } from 'discord.js';
+import { Client, MessageEmbed, version } from 'discord.js';
 
 export default async (_Discord: typeof import('discord.js'), client: Client, warning: string) => {
     console.warn(warning);
-    errorLog(warning, client);
+
+    const embed = new MessageEmbed()
+        .setColor('YELLOW')
+        .setTitle(`${client.user.username} Client-Emitted Warning`)
+        .setDescription(`\`\`\`${warning}\`\`\``)
+        .setTimestamp()
+        .setFooter(`Warning occurred while running discord.js v${version}.`);
+
+    errorLog(embed, client, { noWebhook: true });
 };
