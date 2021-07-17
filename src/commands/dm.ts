a1f37d7a2a48ba0fa4acc8d30f72a1cd0b4a04e8
Costpap <costpapcostpap7@gmail.com>
2021-07-17 23:36:11 +0300

    refactor(cmds): rewrite dm.ts Rewrite the dm.ts command for better readability, clean up old code, and repeat certain code less. Close #42.

--- src/commands/dm.ts
+++ src/commands/dm.ts
@@ -19,11 +19,23 @@ export default {
         }
         if (message.channel.type !== 'dm') message.delete();
 
-        const embed = new Discord.MessageEmbed().setColor(0x6293f5).setTimestamp();
+        if (args[1] === 'embed') {
+            /**
+             * Determines what number should be used in order to slice arguments.
+             */
+            let sliceNumber: number;
+            if (args[2] === 'name') sliceNumber = 3;
+            else sliceNumber = 2;
 
-        if (args[1] === 'embed' && args[2] === 'name') {
-            embed.setDescription(args.slice(3).join(' '));
-            embed.setAuthor(message.author.tag, message.author.displayAvatarURL({ format: 'png', dynamic: true }));
+            const embed = new Discord.MessageEmbed()
+                .setColor(0x6293f5)
+                .setDescription(args.slice(sliceNumber).join(' '))
+                .setTimestamp();
+
+            if (sliceNumber === 3) {
+                embed.setAuthor(message.author.tag, message.author.displayAvatarURL({ format: 'png', dynamic: true }));
+            }
+
             try {
                 dmUser.send(embed);
                 const sentMessage: Message = await message.channel.send(
@@ -36,22 +48,6 @@ export default {
                     error,
                 );
                 return message.channel.send(`❌ Could not send message to ${dmUser.tag}`);
-            }
-        }
-        if (args[1] === 'embed') {
-            embed.setDescription(args.slice(2).join(' '));
-            try {
-                dmUser.send(embed);
-                const sentMessage: Message = await message.channel.send(
-                    `✅ Successfully sent DM to **${dmUser.tag}**!`,
-                );
-                return sentMessage.delete({ timeout: 5000 });
-            } catch (error) {
-                console.error(
-                    `Could not send ${message.author.tag}'s (${message.author.id}) DM to ${dmUser.tag} (${dmUser.id}):\n`,
-                    error,
-                );
-                return message.channel.send(`❌ Could not send message to **${dmUser.tag}**.`);
             }
         }
 
