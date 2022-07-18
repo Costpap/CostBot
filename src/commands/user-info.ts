a0bac00a2a48a4343f6fb463bf3c04bcf9d6e977
Suspense <suspensetm@gmail.com>
2022-07-18 20:10:04 +0200

    F for this newline
    Co-authored-by: Constantinos <37239383+Costpap@users.noreply.github.com>
    
    Signed-off-by: Suspense <suspensetm@gmail.com>

--- src/commands/user-info.ts
+++ src/commands/user-info.ts
@@ -15,7 +15,6 @@ export default {
                 .setRequired(false),
         )
         .setDMPermission(false),
-        
     run: async (interaction: ChatInputCommandInteraction) => {
         // Typeguard in order to ensure having access to ChatInputCommand interaction options.
         if (!interaction.isChatInputCommand()) return;
