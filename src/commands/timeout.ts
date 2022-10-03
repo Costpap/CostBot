afe85b4884f22fe4f50fe304f38e505c278f1bf0
Ádám Lőrincz <adam7605@easv365.dk>
2022-10-03 16:35:40 +0200

    Linter ran again and corrected formatting
    Did not seem to correct formatting errors the first time...
    
    Signed-off-by: Ádám Lőrincz <adam7605@easv365.dk>

--- src/commands/timeout.ts
+++ src/commands/timeout.ts
@@ -7,27 +7,32 @@ export default {
     data: new SlashCommandBuilder()
         .setName('timeout')
         .setDescription('Manages the timeout of the mentioned user.')
-            .addSubcommand(subcommand => subcommand.setName('add')
+        .addSubcommand((subcommand) =>
+            subcommand
+                .setName('add')
                 .setDescription('Times out a user.')
-                .addUserOption((option) => 
-                    option.setName('user')
-                    .setDescription('User to time out')
-                    .setRequired(true))
-                .addIntegerOption(option => //TODO: There are fixed times for this, so this should be a choice?
-                    option.setName('duration') 
-                    .setDescription('Duration of the timeout (in minutes)')
-                    .setRequired(true)) 
-                .addStringOption(option =>
-                    option.setName('reason')
-                    .setDescription('Reason for timing out the user')
-                    .setRequired(true))) 
-            .addSubcommand(subcommand => subcommand.setName('remove')
+                .addUserOption((option) => option.setName('user').setDescription('User to time out').setRequired(true))
+                .addIntegerOption(
+                    (
+                        option, //TODO: There are fixed times for this, so this should be a choice?
+                    ) =>
+                        option
+                            .setName('duration')
+                            .setDescription('Duration of the timeout (in minutes)')
+                            .setRequired(true),
+                )
+                .addStringOption((option) =>
+                    option.setName('reason').setDescription('Reason for timing out the user').setRequired(true),
+                ),
+        )
+        .addSubcommand((subcommand) =>
+            subcommand
+                .setName('remove')
                 .setDescription('Removes the timeout of a user.')
-                .addUserOption((option) => 
-                    option.setName('user')
-                    .setDescription('User to remove the timeout of')
-                    .setRequired(true))) 
-        ,
+                .addUserOption((option) =>
+                    option.setName('user').setDescription('User to remove the timeout of').setRequired(true),
+                ),
+        ),
     run: async (interaction: ChatInputCommandInteraction) => {
         if (!interaction.isChatInputCommand()) return;
 
@@ -57,7 +62,8 @@ export default {
             return interaction.reply({ content: "Aww, please don't time out yourself! 💖", ephemeral: true });
         }
 
-        if (member.permissions.has('Administrator') || member.id === interaction.guild.ownerId) { //Permission check for Admins and server owners - they cannot be timed out
+        if (member.permissions.has('Administrator') || member.id === interaction.guild.ownerId) {
+            //Permission check for Admins and server owners - they cannot be timed out
             return interaction.reply({
                 content: '❌ I cannot time out this user! \n**Admins and the server owner cannot be timed out.**',
                 ephemeral: true,
@@ -65,10 +71,13 @@ export default {
         }
 
         if (interaction.options.getSubcommand() === 'add') {
-            let response = `⏳ Timed out \`${member.user.tag} (${member.id})\`.`;
+            const response = `⏳ Timed out \`${member.user.tag} (${member.id})\`.`;
 
             try {
-                await member.timeout(interaction.options.getInteger('duration') * 60 * 1000, interaction.options.getString('reason'))
+                await member.timeout(
+                    interaction.options.getInteger('duration') * 60 * 1000,
+                    interaction.options.getString('reason'),
+                );
                 interaction.reply({ content: response, ephemeral: true });
             } catch (error) {
                 console.error(error);
@@ -82,20 +91,20 @@ export default {
         }
 
         if (interaction.options.getSubcommand() === 'remove') {
-            let response = `⌛ Timeout of \`${member.user.tag} (${member.id})\` has been removed.`;
+            const response = `⌛ Timeout of \`${member.user.tag} (${member.id})\` has been removed.`;
 
             try {
-                await member.timeout(null)
+                await member.timeout(null);
                 interaction.reply({ content: response, ephemeral: true });
             } catch (error) {
                 console.error(error);
                 return interaction.reply({
-                    content: `❌ I encountered an error while trying remove the timeout of \`${member.user.tag}\`: \n\`\`\`${
-                        error?.message || error
-                    }\`\`\``,
+                    content: `❌ I encountered an error while trying remove the timeout of \`${
+                        member.user.tag
+                    }\`: \n\`\`\`${error?.message || error}\`\`\``,
                     ephemeral: true,
                 });
             }
         }
-    }
-}
+    },
+};
