15785f63935d78b85c2400943f991e3d639c525a
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2023-02-11 16:30:15 -0700

    Update anime.ts
    Signed-off-by: PiPinecone <58414056+PiPinecone@users.noreply.github.com>

--- src/commands/anime.ts
+++ src/commands/anime.ts
@@ -6,16 +6,16 @@ export default {
         .setDescription('important info'),
     run: async (interaction: ChatInputCommandInteraction, client: Client) => {
         const animeStrings: string[] = [
-            "Baka! Don't you know that anime is more than just a cartoon? It's a lifestyle, a culture, a passion. Get ready to become a true weeb with me by your side.",
-            "anime",
-            "ANIME ANIME ANIME ANIME ANIME ANIME ANIME ANIME ANIME ANIME ANIME ANIME ANIME ANIME ANIME ANIME ANIME ANIME",
-            "Costpap is a weeb",
-            "My oh my! I love anime",
-            "OwO! What's this? Did someone say anime? I'm all ears! Let's talk about our favorite characters, ships, and moments. Who's in for a good kawaii time?",
-            "blushes M-My apologies, I seem to have become a little too excited. Let's get back to talking about anime. Who's your favorite waifu or husbando? I'm rooting for (insert character here).",
-            "UwU, hewwo evwyone! I'm youw favowite anime bot, hewe to bwing you awl the wates and gweatest nyews fwom the anime wowld. Who's weady to have some fun?",
-            "UwU, don't be shy, come and talk to me. I pwomise I won't bite. Wewl, unless you'we into that kind of thing. Then I might nibble a wittle.",
-            "I am an anime bot",
+            `Baka! Don't you know that anime is more than just a cartoon? It's a lifestyle, a culture, a passion. Get ready to become a true weeb with me by your side.`,
+            `anime`,
+            `ANIME ANIME ANIME ANIME ANIME ANIME ANIME ANIME ANIME ANIME ANIME ANIME ANIME ANIME ANIME ANIME ANIME ANIME`,
+            `Costpap is a weeb`,
+            `My oh my! I love anime`,
+            `OwO! What's this? Did someone say anime? I'm all ears! Let's talk about our favorite characters, ships, and moments. Who's in for a good kawaii time?`,
+            `blushes M-My apologies, I seem to have become a little too excited. Let's get back to talking about anime. Who's your favorite waifu or husbando? I'm rooting for (insert character here).`,
+            `UwU, hewwo evwyone! I'm youw favowite anime bot, hewe to bwing you awl the wates and gweatest nyews fwom the anime wowld. Who's weady to have some fun?`,
+            `UwU, don't be shy, come and talk to me. I pwomise I won't bite. Wewl, unless you'we into that kind of thing. Then I might nibble a wittle.`,
+            `I am an anime bot`,
         ];
         /**
          * Randomly picks a string from the `animeStrings` array in order to send to the user.
